0021e1c16b70ae83f228301cf2e3fdf77ed1f639
macbook <yorikyorik25@gmail.com>
2022-09-26 20:49:58 +0300

    #2 add lib requests, validation data

--- tests/something_test.py
+++ tests/something_test.py
@@ -1,3 +1,11 @@
+import requests
+from configuration import SERVICE_URL
+from src.enums.global_enums import GlobalErrorMassages
 
-def test_equal():
-    assert 1 == 1, "hi"
+def test_getting_posts():
+    resource = requests.get(url=SERVICE_URL)
+    received_posts = resource.json()
+
+    assert resource.status_code == 200, GlobalErrorMassages.WRONG_STATUS_CODE.value
+    assert len(received_posts) == 3, GlobalErrorMassages.WRONG_ELEMENT_COUNT.value
+    #print(resource.json())
